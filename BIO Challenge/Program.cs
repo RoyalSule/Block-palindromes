8d570397efa9d2c9518696cdacf765c519da8856
Royal Sule <SuleR@StPaulsSchool.org.uk>
2021-11-09 12:07:17 +0000

    Add project files.

---
+++ BIO Challenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BIO_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
